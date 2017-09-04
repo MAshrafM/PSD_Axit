8224b0d6a9b059dcdab82257dfea3c7ee29f8bb4
MAshraf <ee.mashraf@gmail.com>
2017-09-04 18:55:06 +0300

    responsive

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,12 @@
 'use strict';
 $(function (){
+  //Trigger Nice Scroll Plugin
+  $("html").niceScroll({
+    cursorcolor: '#ff8b38',
+    cursorwidth: 10,
+    cursorborderradius: 0,
+    cursorborder: '1px solid #ff8b38'
+  });
   // Scroll
   $(window).scroll(function(){
     let navbar = $(".navbar");
@@ -14,6 +21,17 @@ $(function (){
       }
     }
   });
+  $(".nav a").click(function(){
+    let dest = $(this).data("go");
+    $("html, body").animate({
+      scrollTop: $(dest).offset().top
+    }, 1000);
+  })
+  $(".top").click(function(){
+    $("html, body").animate({
+      scrollTop: $("body").offset().top
+    }, 1000);
+  })
   // Tabs
   $(".tab-switch li").click(function(){
     $(this).addClass("selected").siblings().removeClass("selected");
